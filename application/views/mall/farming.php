dd095f340adb6b0919fe9dbf6db24af801cacae2
lvlinlin <549663504@qq.com>
2016-10-16 21:49:42 +0800

    sdsdgsfg

--- application/views/mall/farming.php
+++ application/views/mall/farming.php
@@ -20,7 +20,7 @@
             <ul id="productContainer" class="navContent">
                 <?php foreach ($list as $v){ ?>
                     <li class="">
-                        <img src="<?php echo imgUrl($v['picture']) ?>"/>
+                        <img src="<?php echo imgUrl($v['picture']) ?>" height="300" style="clear: both;display: block;margin:auto; "/>
                     </li>
                     <br/>
                     <li class="">
@@ -34,6 +34,13 @@
                         </video>
                     </li>
                     <?php endif;?>
+                    <li>
+                        <?php if(time()<$v['outtime']):?>
+                        <script language="javascript" type="text/javascript"> var interval = 300;window.setInterval(function(){ShowCountDown(<?php echo date("Y",$v['outtime']);?>,<?php echo date("m",$v['outtime']);?>,<?php echo date("d",$v['outtime']);?>,'divdown1');}, interval);</script>
+                        <label style="color: red;font-size: 18px;">倒计时:</label><div id="divdown1"></div>
+                        <?php endif;?>
+                        </li>
+                    <br/>
                     <a class="buy_now" href="/mall/detail/<?php echo $v['goods_id'];?>">我要领养</a>
                 <?php }; ?>
             </ul>
@@ -48,4 +55,22 @@
     <!-- 底部导航 -->
     <?php $this->load->view('mall/_footer'); ?>
 </body>
+<script language="javascript" type="text/javascript">
+
+    function ShowCountDown(year,month,day,divname)
+    {
+        var now = new Date();
+        var endDate = new Date(year, month-1, day);
+        var leftTime=endDate.getTime()-now.getTime();
+        var leftsecond = parseInt(leftTime/1000);
+//var day1=parseInt(leftsecond/(24*60*60*6));
+        var day1=Math.floor(leftsecond/(60*60*24));
+        var hour=Math.floor((leftsecond-day1*24*60*60)/3600);
+        var minute=Math.floor((leftsecond-day1*24*60*60-hour*3600)/60);
+        var second=Math.floor(leftsecond-day1*24*60*60-hour*3600-minute*60);
+        var cc = document.getElementById(divname);
+        cc.innerHTML = day1+"天"+hour+"小时"+minute+"分"+second+"秒";
+    }
+
+</script>
 </html>
